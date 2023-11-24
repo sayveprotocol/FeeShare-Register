b79be22a7246a102de78bfd69092513d55361ed1
Sayve Protocol <92076339+sayveprotocol@users.noreply.github.com>
2023-11-24 12:10:57 -0800

    Create register.js

---
+++ register.js
@@ -0,0 +1,31 @@
+import { LCDClient, MnemonicKey, MsgRegisterFeeShare } from "@terra-money/feather.js";
+
+// Prepare environment clients, accounts and wallets
+const lcd = LCDClient.fromDefaultConfig("testnet");
+const mnemonic = new MnemonicKey({ mnemonic: "..." });
+const deployerAddr = mnemonic.accAddress("terra");
+const withdrawerAddr = mnemonic.accAddress("terra");
+const wallet = lcd.wallet(mnemonic);
+const contractAddr = "terra1eaxcahzxp0x8wqejqjlqaey53tp06l728qad6z395lyzgl026qkq20xj43";
+
+
+(async () => {
+    try {
+        // Submit a transaction to register the feeshare 
+        let tx = await wallet.createAndSignTx({
+            msgs: [new MsgRegisterFeeShare(
+                contractAddr,
+                deployerAddr,
+                withdrawerAddr,
+            )],
+            chainID: "pisco-1",
+            memo: "Registering feeshare #TerraDevs",
+        });
+        let result = await lcd.tx.broadcastSync(tx, "test-1");
+
+        console.log("Transaction Hash", result.txhash)
+    }
+    catch (e) {
+        console.log(e)
+    }
+})()
